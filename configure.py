74c035b60f592f8243e4b6d39381ed8858091b8d
Marat Dukhan <marat.dukhan@amd.com>
2014-05-30 13:35:58 -0700

    Build: do not build JavaScript with Ninja

--- configure.py
+++ configure.py
@@ -7,7 +7,6 @@ import ninja_syntax
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option("--with-jsdoc", dest="jsdoc_compiler", default=os.getenv("JSDOC_COMPILER"))
     parser.add_option("--with-nacl-sdk", dest="nacl_sdk", default=os.getenv("NACL_SDK_ROOT"))
     parser.add_option("--pepper-api", dest="pepper_api", default="canary")
     options, _ = parser.parse_args()
@@ -18,37 +17,20 @@ if __name__ == '__main__':
         ninja = ninja_syntax.Writer(buildfile)
 
         # Variables
-        if options.jsdoc_compiler:
-            ninja.variable('jsdoc_compiler', options.jsdoc_compiler)
         if options.nacl_sdk:
             ninja.variable('nacl_sdk_dir', os.path.join(options.nacl_sdk, 'pepper_' + options.pepper_api))
             if os.name == 'nt':
                 ninja.variable('pnacl_toolchain_dir', '$nacl_sdk_dir/toolchain/win_pnacl')
-            else:
-                ninja.variable('pnacl_toolchain_dir', '$nacl_sdk_dir/toolchain/linux_pnacl')
-            if os.name == 'nt':
                 ninja.variable('pnacl_cc', '$pnacl_toolchain_dir/bin/pnacl-clang.bat')
                 ninja.variable('pnacl_cxx', '$pnacl_toolchain_dir/bin/pnacl-clang++.bat')
                 ninja.variable('pnacl_finalize', '$pnacl_toolchain_dir/bin/pnacl-finalize.bat')
             else:
+                ninja.variable('pnacl_toolchain_dir', '$nacl_sdk_dir/toolchain/linux_pnacl')
                 ninja.variable('pnacl_cc', '$pnacl_toolchain_dir/bin/pnacl-clang')
                 ninja.variable('pnacl_cxx', '$pnacl_toolchain_dir/bin/pnacl-clang++')
                 ninja.variable('pnacl_finalize', '$pnacl_toolchain_dir/bin/pnacl-finalize')
 
         # Rules
-	if os.name == 'nt':
-            ninja.rule('BROWSERIFY', 'cmd /K "browserify $in --outfile $min -p [minifyify --map $map --output $map]"',
-                description='BROWSERIFY $out')
-            ninja.rule('BROWSERIFY_STANDALONE', 'cmd /K "browserify --standalone $module --entry $entry --outfile $min --plugin [minifyify --map $map --output $map]"',
-                description='BROWSERIFY MODULE $module')
-	else:
-            ninja.rule('BROWSERIFY', 'browserify $in --outfile $min -p [minifyify --map $map --output $map]',
-                description='BROWSERIFY $out')
-            ninja.rule('BROWSERIFY_STANDALONE', 'browserify --standalone $module --entry $entry --outfile $min --plugin [minifyify --map $map --output $map]',
-                description='BROWSERIFY MODULE $module')
-        if options.jsdoc_compiler:
-            ninja.rule('COMPILE_JSDOC', '$jsdoc_compiler $in -d $output_directory',
-                description='JSDOC $in')
         if options.nacl_sdk:
             ninja.rule('COMPILE_PNACL_C', '$pnacl_cc -o $out -c $in -MMD -MF $out.d $optflags $cflags',
                 deps='gcc', depfile='$out.d',
@@ -62,23 +44,6 @@ if __name__ == '__main__':
                 description='FINALIZE[PNaCl] $out')
 
         # Build targets
-        js_source_dir = os.path.join(root_dir, 'lib')
-        js_sources = [os.path.join(root_dir, path) for path in glob.glob(os.path.join(js_source_dir, "*.js"))]
-        js_source_min = os.path.join(root_dir, 'numjs.min.js')
-        js_source_map = os.path.join(root_dir, 'numjs.map')
-        ninja.build([js_source_min, js_source_map], 'BROWSERIFY_STANDALONE', js_sources,
-            variables={'module': 'numjs', 'entry': 'lib/numjs.js', 'min': js_source_min, 'map': js_source_map})
-        js_test_source_dir = os.path.join(root_dir, 'test')
-        js_test_sources = [os.path.join(root_dir, path) for path in glob.glob(os.path.join(js_test_source_dir, "*.test.js"))]
-        js_test_source_min = os.path.join(root_dir, 'test.min.js')
-        js_test_source_map = os.path.join(root_dir, 'test.map')
-        ninja.build([js_test_source_min, js_test_source_map], 'BROWSERIFY', js_test_sources,
-            variables={'min': js_test_source_min, 'map': js_test_source_map})
-        if options.jsdoc_compiler:
-            ninja.build(os.path.join(root_dir, 'doc', 'index.html'), 'COMPILE_JSDOC', js_sources,
-                variables={'output_directory': 'doc'})
-        
-
         if options.nacl_sdk:
             c_source_dir = os.path.join(root_dir, "lib", "nacl")
             c_build_dir = os.path.join(root_dir, "build", "nacl")
