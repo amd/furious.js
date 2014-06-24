b3fdc30a36bddc2e3ae08b730a6a0aec41d41787
Marat Dukhan <marat.dukhan@amd.com>
2014-06-24 10:56:59 -0700

    Context: rename toArray method into get
    The method now can return numbers, and the name toArray is too confusing

--- test/NDArray.test.js
+++ test/NDArray.test.js
@@ -34,17 +34,17 @@ describe("NDArray", function(){
 		});
 		it("Rearranges data", function(done){
 			var x = context.linspace(1, 8, 8).reshape([2, 2, 2]);
-			x.toArray(function(result){
+			x.get(function(result){
 				expect(result).to.deep.equal([[[ 1,  2], [ 3,  4]],
 											  [[ 5,  6], [ 7,  8]]]);
 				done();
 			});
 		});
 	});
-	describe("toArray", function(){
+	describe("get", function(){
 		it("Works with 1-dimensional array", function(done){
 			var x = context.array([42, 10]);
-			x.toArray(function(result){
+			x.get(function(result){
 				expect(result).to.deep.equal([42, 10]);
 				done();
 			});
@@ -54,7 +54,7 @@ describe("NDArray", function(){
 						 [11, 10,  8,  9,  7],
 						 [ 6, 12,  4, 14, 15]];
 			var x = context.array(array);
-			x.toArray(function(result){
+			x.get(function(result){
 				expect(result).to.deep.equal(array);
 				done();
 			});
@@ -66,7 +66,7 @@ describe("NDArray", function(){
 				var x = context.array([1, 4, 9]);
 				var y = context.array([8, -1, 10]);
 				var z = x.add(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([9, 3, 19]);
 					done();
 				});
@@ -75,7 +75,7 @@ describe("NDArray", function(){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = context.array([[8, -1], [10, -21]]);
 				var z = x.add(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([[9, 3], [19, -38]]);
 					done();
 				});
@@ -85,7 +85,7 @@ describe("NDArray", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
 				var z = x.add(-7);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([-6, -3, 2]);
 					done();
 				});
@@ -93,7 +93,7 @@ describe("NDArray", function(){
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[1, 4], [9, -17]]);
 				var z = x.add(42);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([[43, 46], [51, 25]]);
 					done();
 				});
@@ -106,7 +106,7 @@ describe("NDArray", function(){
 				var x = context.array([1, 4, 9]);
 				var y = context.array([8, -1, 10]);
 				var z = x.sub(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([-7, 5, -1]);
 					done();
 				});
@@ -115,7 +115,7 @@ describe("NDArray", function(){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = context.array([[8, -1], [10, -21]]);
 				var z = x.sub(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([[-7, 5], [-1, 4]]);
 					done();
 				});
@@ -125,7 +125,7 @@ describe("NDArray", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
 				var y = x.sub(-7);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([8, 11, 16]);
 					done();
 				});
@@ -133,7 +133,7 @@ describe("NDArray", function(){
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = x.sub(42);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([[-41, -38], [-33, -59]]);
 					done();
 				});
@@ -146,7 +146,7 @@ describe("NDArray", function(){
 				var x = context.array([1, 4, 9]);
 				var y = context.array([8, -1, 10]);
 				var z = x.mul(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([8, -4, 90]);
 					done();
 				});
@@ -155,7 +155,7 @@ describe("NDArray", function(){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = context.array([[8, -1], [10, -21]]);
 				var z = x.mul(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([[8, -4], [90, 357]]);
 					done();
 				});
@@ -165,7 +165,7 @@ describe("NDArray", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
 				var y = x.mul(-10);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([-10, -40, -90]);
 					done();
 				});
@@ -173,7 +173,7 @@ describe("NDArray", function(){
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = x.mul(10);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([[10, 40], [90, -170]]);
 					done();
 				});
@@ -186,7 +186,7 @@ describe("NDArray", function(){
 				var x = context.array([1, 4, 9]);
 				var y = context.array([2, -4, 8]);
 				var z = x.div(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([0.5, -1, 1.125]);
 					done();
 				});
@@ -195,7 +195,7 @@ describe("NDArray", function(){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = context.array([[-2, 4], [-8, 16]]);
 				var z = x.div(y);
-				z.toArray(function(result){
+				z.get(function(result){
 					expect(result).to.deep.equal([[-0.5, 1], [-1.125, -1.0625]]);
 					done();
 				});
@@ -205,14 +205,14 @@ describe("NDArray", function(){
 			it("Correct result for 1-dimensional arrays", function(){
 				var x = context.array([1, 4, 9]);
 				var y = x.div(-2);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([-0.5, -2, -4.5]);
 				});
 			});
 			it("Correct result for 2-dimensional arrays", function(){
 				var x = context.array([[1, 4], [9, -17]]);
 				var y = x.div(-4);
-				y.toArray(function(result){
+				y.get(function(result){
 					expect(result).to.deep.equal([[-0.25, -1], [-2.25, 4.25]]);
 				});
 			});
@@ -222,14 +222,14 @@ describe("NDArray", function(){
 		describe("All elements", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
-				x.min().toArray(function(result) {
+				x.min().get(function(result) {
 					expect(result).to.equal(1);
 					done();
 				});
 			});
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[-2, 4], [-8, 16]]);
-				x.min().toArray(function(result) {
+				x.min().get(function(result) {
 					expect(result).to.equal(-8);
 					done();
 				});
@@ -244,7 +244,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 0", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.min(0).toArray(function(result){
+				x.min(0).get(function(result){
 					expect(result).to.deep.equal([[ 1,  2,  3,  4],
 												  [ 5,  6,  7,  8],
 												  [ 9, 10, 11, 12]]);
@@ -253,7 +253,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 1", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.min(1).toArray(function(result){
+				x.min(1).get(function(result){
 					expect(result).to.deep.equal([[  1,  2,  3,  4],
 												  [ 13, 14, 15, 16]]);
 					done();
@@ -261,7 +261,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 2", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.min(2).toArray(function(result){
+				x.min(2).get(function(result){
 					expect(result).to.deep.equal([[  1,  5,  9],
 												  [ 13, 17, 21]]);
 					done();
@@ -273,14 +273,14 @@ describe("NDArray", function(){
 		describe("All elements", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
-				x.max().toArray(function(result) {
+				x.max().get(function(result) {
 					expect(result).to.equal(9);
 					done();
 				});
 			});
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[-2, 4], [-8, 16]]);
-				x.max().toArray(function(result) {
+				x.max().get(function(result) {
 					expect(result).to.equal(16);
 					done();
 				});
@@ -295,7 +295,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 0", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.max(0).toArray(function(result){
+				x.max(0).get(function(result){
 					expect(result).to.deep.equal([[ 13, 14, 15, 16],
 												  [ 17, 18, 19, 20],
 												  [ 21, 22, 23, 24]]);
@@ -304,7 +304,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 1", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.max(1).toArray(function(result){
+				x.max(1).get(function(result){
 					expect(result).to.deep.equal([[  9, 10, 11, 12],
 												  [ 21, 22, 23, 24]]);
 					done();
@@ -312,7 +312,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 2", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.max(2).toArray(function(result){
+				x.max(2).get(function(result){
 					expect(result).to.deep.equal([[  4,  8, 12],
 												  [ 16, 20, 24]]);
 					done();
@@ -324,14 +324,14 @@ describe("NDArray", function(){
 		describe("All elements", function(){
 			it("Correct result for 1-dimensional arrays", function(done){
 				var x = context.array([1, 4, 9]);
-				x.sum().toArray(function (result) {
+				x.sum().get(function (result) {
 					expect(result).to.equal(14);
 					done();
 				});
 			});
 			it("Correct result for 2-dimensional arrays", function(done){
 				var x = context.array([[-2, 4], [-8, 16]]);
-				x.sum().toArray(function (result) {
+				x.sum().get(function (result) {
 					expect(result).to.equal(10);
 					done();
 				});
@@ -346,7 +346,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 0", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.sum(0).toArray(function(result){
+				x.sum(0).get(function(result){
 					expect(result).to.deep.equal([[ 14, 16, 18, 20],
 												  [ 22, 24, 26, 28],
 												  [ 30, 32, 34, 36]]);
@@ -355,7 +355,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 1", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.sum(1).toArray(function(result){
+				x.sum(1).get(function(result){
 					expect(result).to.deep.equal([[ 15,  18,  21,  24],
 												  [ 51,  54,  57,  60]]);
 					done();
@@ -363,7 +363,7 @@ describe("NDArray", function(){
 			});
 			it("Correct result for 3-dimensional arrays, axis 2", function(done){
 				var x = context.linspace(1, 24, 24).reshape([2, 3, 4]);
-				x.sum(2).toArray(function(result){
+				x.sum(2).get(function(result){
 					expect(result).to.deep.equal([[ 10,  26,  42],
 												  [ 58,  74,  90]]);
 					done();
@@ -390,7 +390,7 @@ describe("NDArray", function(){
 		it("Correct value for 1-dimensional arrays", function(done){
 			var x = context.array([2, 5]);
 			var y = context.array([5, 11]);
-			context.dot(x, y).toArray(function(result){
+			context.dot(x, y).get(function(result){
 				expect(result).to.deep.equal(65);
 				done();
 			});
@@ -402,7 +402,7 @@ describe("NDArray", function(){
 								   [67, 74, 51, 58, 40],
 								   [98, 80,  7, 14, 16]]);
 			var z = context.dot(x, y);
-			z.toArray(function(result){
+			z.get(function(result){
 				expect(result).to.deep.equal([[  6316,  6724,  187,  670, 1088],
 											  [ 10220, 10959, 3163, 4052, 3411]]);
 				done();
@@ -440,7 +440,7 @@ describe("array", function(){
 	it("Matches the data of the provided array", function(done){
 		var array = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]];
 		var x = context.array(array);
-		x.toArray(function(result){
+		x.get(function(result){
 			expect(result).to.deep.equal(array);
 			done();
 		});
@@ -457,7 +457,7 @@ describe("linspace", function(){
 		var start = 50;
 		var stop = 99;
 		var x = context.linspace(start, stop);
-		x.toArray(function(result) {
+		x.get(function(result) {
 			for (var i = 0; i < result.length; i++) {
 				expect(result[i]).to.equal(start+i);
 			}
@@ -470,7 +470,7 @@ describe("linspace", function(){
 		});
 		it("Does not contain the right endpoint", function(done){
 			var x = context.linspace(-1, 1, 1000, false);
-			x.toArray(function(result) {
+			x.get(function(result) {
 				expect(result[result.length - 1]).to.not.equal(1);
 				done();
 			});
@@ -481,7 +481,7 @@ describe("neg", function() {
 	it("Correct result for 2-dimensional array", function(done){
 		var x = context.array([1, -7.5, 0, -15]);
 		var y = context.neg(x);
-		y.toArray(function(result){
+		y.get(function(result){
 			expect(result).to.deep.equal([-1, 7.5, -0, 15]);
 			done();
 		});
@@ -491,7 +491,7 @@ describe("abs", function() {
 	it("Correct result for 2-dimensional array", function(done){
 		var x = context.array([1, -7.5, 0, -15]);
 		var y = context.abs(x);
-		y.toArray(function(result){
+		y.get(function(result){
 			expect(result).to.deep.equal([1, 7.5, 0, 15]);
 			done();
 		});
@@ -500,7 +500,7 @@ describe("abs", function() {
 describe("exp", function() {
 	it("Correct result for 1-dimensional newly created output array", function(done){
 		var x = context.array([1, -1, 0]);
-		context.exp(x).toArray(function(result) {
+		context.exp(x).get(function(result) {
 			expect(result[0]).to.be.closeTo(Math.exp(1), Math.exp(1) * 2.2204460492503130808472633361816E-16 * 3);
 			expect(result[1]).to.be.closeTo(Math.exp(-1), Math.exp(-1) * 2.2204460492503130808472633361816E-16 * 3);
 			expect(result[2]).to.equal(1);
@@ -511,7 +511,7 @@ describe("exp", function() {
 describe("log", function() {
 	it("Correct result for 1-dimensional newly created output array", function(done){
 		var x = context.array([1, 3, 10]);
-		context.log(x).toArray(function(result) {
+		context.log(x).get(function(result) {
 			expect(result[0]).to.equal(0);
 			expect(result[1]).to.be.closeTo(Math.log(3), Math.log(3) * 2.2204460492503130808472633361816E-16 * 3);
 			expect(result[2]).to.be.closeTo(Math.log(10), Math.log(10) * 2.2204460492503130808472633361816E-16 * 3);
@@ -522,7 +522,7 @@ describe("log", function() {
 describe("sqrt", function() {
 	it("Correct result for 1-dimensional newly created output array", function(done){
 		var x = context.array([0, 0.25, 1, 9, 10]);
-		context.sqrt(x).toArray(function(result) {
+		context.sqrt(x).get(function(result) {
 			expect(result[0]).to.equal(0);
 			expect(result[1]).to.equal(0.5);
 			expect(result[2]).to.equal(1);
@@ -535,7 +535,7 @@ describe("sqrt", function() {
 describe("square", function() {
 	it("Correct result for 1-dimensional newly created output array", function(done){
 		var x = context.array([-2, 0, 0.5, 1, 3]);
-		context.square(x).toArray(function(result) {
+		context.square(x).get(function(result) {
 			expect(result[0]).to.equal(4);
 			expect(result[1]).to.equal(0);
 			expect(result[2]).to.equal(0.25);
