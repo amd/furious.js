be00908c36c28a6fce459e3888222f8da3c816da
Marat Dukhan <marat.dukhan@amd.com>
2014-06-27 15:42:39 -0700

    Website: Erase everything

--- lib/nacl/IdMap.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <ppapi/c/pp_instance.h>
-
-extern void* FJS_GetPointerFromId(PP_Instance instance, int32_t id);
-extern void FJS_ReleaseId(PP_Instance instance, int32_t id);
-extern void FJS_AllocateId(PP_Instance instance, int32_t id, void* pointer);
