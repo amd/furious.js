edaaf35676bbf27e7ecf38d5f83a7c7f75dafd0f
Marat Dukhan <marat.dukhan@amd.com>
2014-07-02 10:50:28 -0700

    Test: refactor unit tests to be spec-like

---
+++ test/Context.test.js
@@ -0,0 +1,507 @@
+var furious = require("../lib/furious.js");
+var expect = require("chai").expect;
+
+var context = null;
+before(function(done) {
+	furious.init(function(ctx) {
+		context = ctx;
+		done();
+	});
+});
+
+describe("Context", function(){
+	describe("empty", function(){
+		it("Creates array with specified shape", function() {
+			var x = context.empty(42);
+			var y = context.empty([42]);
+			var z = context.empty([4, 2]);
+			expect(x.shape).to.deep.equal([42]);
+			expect(y.shape).to.deep.equal([42]);
+			expect(z.shape).to.deep.equal([4, 2]);
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with specified data type (f64 by default)", function() {
+			var x = context.empty([4, 2]);
+			var y = context.empty([4, 2], new furious.DataType("f64"));
+			var z = context.empty([4, 2], new furious.DataType("f32"));
+			expect(x.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(y.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(z.dataType.equals(new furious.DataType("f32"))).to.be.true;
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+	});
+	describe("zeros", function(){
+		it("Creates array with specified shape", function() {
+			var x = context.zeros(42);
+			var y = context.zeros([42]);
+			var z = context.zeros([4, 2]);
+			expect(x.shape).to.deep.equal([42]);
+			expect(y.shape).to.deep.equal([42]);
+			expect(z.shape).to.deep.equal([4, 2]);
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with specified data type (f64 by default)", function() {
+			var x = context.zeros([4, 2]);
+			var y = context.zeros([4, 2], new furious.DataType("f64"));
+			var z = context.zeros([4, 2], new furious.DataType("f32"));
+			expect(x.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(y.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(z.dataType.equals(new furious.DataType("f32"))).to.be.true;
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with all elements initialized to zero", function(done) {
+			var x = context.zeros([3, 2], new furious.DataType("f64"));
+			var y = context.zeros([2, 3], new furious.DataType("f32"));
+			context.get(x, y, function(x, y) {
+				expect(x).to.deep.equal([[0.0, 0.0],
+				                         [0.0, 0.0],
+				                         [0.0, 0.0]]);
+				expect(y).to.deep.equal([[0.0, 0.0, 0.0],
+				                         [0.0, 0.0, 0.0]]);
+				done();
+			});
+		});
+	});
+	describe("ones", function(){
+		it("Creates array with specified shape", function() {
+			var x = context.ones(42);
+			var y = context.ones([42]);
+			var z = context.ones([4, 2]);
+			expect(x.shape).to.deep.equal([42]);
+			expect(y.shape).to.deep.equal([42]);
+			expect(z.shape).to.deep.equal([4, 2]);
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with specified data type (f64 by default)", function() {
+			var x = context.ones([4, 2]);
+			var y = context.ones([4, 2], new furious.DataType("f64"));
+			var z = context.ones([4, 2], new furious.DataType("f32"));
+			expect(x.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(y.dataType.equals(new furious.DataType("f64"))).to.be.true;
+			expect(z.dataType.equals(new furious.DataType("f32"))).to.be.true;
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with all elements initialized to one", function(done) {
+			var x = context.ones([3, 2], new furious.DataType("f64"));
+			var y = context.ones([2, 3], new furious.DataType("f32"));
+			context.get(x, y, function(x, y) {
+				expect(x).to.deep.equal([[1.0, 1.0],
+				                         [1.0, 1.0],
+				                         [1.0, 1.0]]);
+				expect(y).to.deep.equal([[1.0, 1.0, 1.0],
+				                         [1.0, 1.0, 1.0]]);
+				done();
+			});
+		});
+	});
+	describe("array", function(){
+		it("Creates array of the same length as the provided array", function(){
+			var x = context.array([0, 1]);
+			var y = context.array([[0, 1],
+			                       [2, 3],
+			                       [3, 4]]);
+			expect(x.length).to.equal(2);
+			expect(y.length).to.equal(6);
+			x.invalidate();
+			y.invalidate();
+		});
+		it("Creates array of the same shape as the provided array", function(){
+			var x = context.array([0, 1]);
+			var y = context.array([[0, 1],
+			                       [2, 3],
+			                       [3, 4]]);
+			var z = context.array([[[1, 2, 3], [ 4,  5,  6]],
+			                       [[7, 8, 9], [10, 11, 12]]]);
+			expect(x.shape).to.deep.equal([2]);
+			expect(y.shape).to.deep.equal([3, 2]);
+			expect(z.shape).to.deep.equal([2, 2, 3]);
+			x.invalidate();
+			y.invalidate();
+			z.invalidate();
+		});
+		it("Creates array with the same data as the provided array", function(done){
+			var array = [[[1, 2, 3], [ 4,  5,  6]],
+			             [[7, 8, 9], [10, 11, 12]]];
+			var x = context.array(array, new furious.DataType("f64"));
+			var y = context.array(array, new furious.DataType("f32"));
+			context.get(x, y, function(x, y) {
+				expect(x).to.deep.equal(array);
+				expect(y).to.deep.equal(array);
+				done();
+			});
+		});
+	});
+	describe("linspace", function(){
+		it("Has length of 50 with default arguments", function(){
+			expect((context.linspace(0, 1)).length).to.equal(50);
+		});
+		it("Has the specified number of samples", function(){
+			expect((context.linspace(0, 1, 243)).length).to.equal(243);
+		});
+		it("Has expected values", function(done){
+			var start = 50;
+			var stop = 99;
+			var x = context.linspace(start, stop);
+			x.get(function(result) {
+				for (var i = 0; i < result.length; i++) {
+					expect(result[i]).to.equal(start+i);
+				}
+				done();
+			});
+		});
+		describe("with includeStop === false", function(){
+			it("Has the specified number of samples", function(){
+				expect((context.linspace(0, 1, 243, false)).length).to.equal(243);
+			});
+			it("Does not contain the right endpoint", function(done){
+				var x = context.linspace(-1, 1, 1000, false);
+				x.get(function(result) {
+					expect(result[result.length - 1]).to.not.equal(1);
+					done();
+				});
+			});
+		});
+	});
+	describe("neg", function() {
+		var xRef = [ 1, -7.5,  0, -15];
+		var yRef = [-1,  7.5, -0,  15];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.neg(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.neg(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with negated elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.neg(x);
+						y.get(function(y) {
+							expect(y).to.deep.equal(yRef);
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with negated elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.neg(x, y);
+						y.get(function(y) {
+							expect(y).to.deep.equal(yRef);
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+	describe("abs", function() {
+		var xRef = [1, -7.5, 0, -15];
+		var yRef = [1,  7.5, 0,  15];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.abs(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.abs(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.abs(x);
+						y.get(function(y) {
+							expect(y).to.deep.equal(yRef);
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.abs(x, y);
+						y.get(function(y) {
+							expect(y).to.deep.equal(yRef);
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+	describe("exp", function() {
+		var xRef = [1, -1, 0];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.exp(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.exp(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.exp(x);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.exp(xRef[k]), Math.exp(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.exp(x, y);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.exp(xRef[k]), Math.exp(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+	describe("log", function() {
+		var xRef = [1, 3, 10];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.log(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.log(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.log(x);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.log(xRef[k]), Math.log(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.log(x, y);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.log(xRef[k]), Math.log(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+	describe("sqrt", function() {
+		var xRef = [0, 0.25, 1, 9, 10];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.sqrt(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.sqrt(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.sqrt(x);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.sqrt(xRef[k]), Math.sqrt(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.sqrt(x, y);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(Math.sqrt(xRef[k]), Math.sqrt(xRef[k]) * 3 * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+	describe("square", function() {
+		var xRef = [-2, 0, 0.5, 1, 3];
+		var dataTypes = ["f32", "f64"];
+
+		describe("With no output array supplied", function() {
+			it("Creates an output array with the same shape as input array", function() {
+				var x = context.ones([2, 3, 4]);
+				var y = context.square(x);
+				expect(y.shape).to.deep.equal([2, 3, 4]);
+				y.invalidate();
+			});
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with the same data type as input array (" + dataType + " data type)", function() {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.square(x);
+						expect(y.dataType.equals(new furious.DataType(dataType))).to.be.true;
+						y.invalidate();
+					});
+				})(dataTypes[i]);
+			}
+
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Creates an output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.square(x);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(xRef[k] * xRef[k], xRef[k] * xRef[k] * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+		describe("With an output array", function() {
+			for (var i = 0; i < dataTypes.length; i++) {
+				(function(dataType) {
+					it("Populates the output array with absolute values of elements (" + dataType + " data type)", function(done) {
+						var x = context.array(xRef, new furious.DataType(dataType));
+						var y = context.ones(x.shape, x.dataType);
+						context.square(x, y);
+						y.get(function(y) {
+							for (var k = 0; k < y.length; k++) {
+								expect(y[k]).to.be.closeTo(xRef[k] * xRef[k], xRef[k] * xRef[k] * x.dataType.epsilon);
+							}
+							done();
+						});
+					});
+				})(dataTypes[i]);
+			}
+		});
+	});
+});
