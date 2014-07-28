97892fe461e5aaed1fd64ad201bc12b5c2df6f33
Marat Dukhan <marat.dukhan@amd.com>
2014-07-28 12:49:45 -0700

    Protobuf: unify coding style

--- lib/nacl/Dispatch.cc
+++ lib/nacl/Dispatch.cc
@@ -18,7 +18,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 	switch (requestType) {
 		case furious::Request_Type_EMPTY_ARRAY:
 		{
-			const furious::EmptyArrayRequest& emptyArrayRequest = request.emptyarrayrequest();
+			const furious::EmptyArrayRequest& emptyArrayRequest = request.empty_array_request();
 			const struct FJS_Shape shape = {
 				.buffer = const_cast<uint32_t*>(emptyArrayRequest.shape().data()),
 				.dimensions = static_cast<uint32_t>(emptyArrayRequest.shape().size())
@@ -30,7 +30,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_DATA_ARRAY:
 		{
-			const furious::DataArrayRequest& dataArrayRequest = request.dataarrayrequest();
+			const furious::DataArrayRequest& dataArrayRequest = request.data_array_request();
 			const struct FJS_Shape shape = {
 				.buffer = const_cast<uint32_t*>(dataArrayRequest.shape().data()),
 				.dimensions = static_cast<uint32_t>(dataArrayRequest.shape().size())
@@ -47,7 +47,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_CONST_ARRAY:
 		{
-			const furious::ConstArrayRequest& constArrayRequest = request.constarrayrequest();
+			const furious::ConstArrayRequest& constArrayRequest = request.const_array_request();
 			const struct FJS_Shape shape = {
 				.buffer = const_cast<uint32_t*>(constArrayRequest.shape().data()),
 				.dimensions = static_cast<uint32_t>(constArrayRequest.shape().size())
@@ -60,7 +60,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_LINSPACE:
 		{
-			const furious::LinspaceRequest& linspaceRequest = request.linspacerequest();
+			const furious::LinspaceRequest& linspaceRequest = request.linspace_request();
 			return FJS_Execute_LinSpace(instance,
 				linspaceRequest.id_out(),
 				linspaceRequest.start(),
@@ -71,7 +71,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_RESHAPE:
 		{
-			const furious::ReshapeRequest& reshapeRequest = request.reshaperequest();
+			const furious::ReshapeRequest& reshapeRequest = request.reshape_request();
 			const FJS_Shape shapeOut = {
 				.buffer = const_cast<uint32_t*>(reshapeRequest.shape_out().data()),
 				.dimensions = static_cast<uint32_t>(reshapeRequest.shape_out().size())
@@ -83,7 +83,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_REPEAT:
 		{
-			const furious::RepeatRequest& repeatRequest = request.repeatrequest();
+			const furious::RepeatRequest& repeatRequest = request.repeat_request();
 			return FJS_Execute_Repeat(instance,
 				repeatRequest.id_a(),
 				repeatRequest.id_out(),
@@ -92,21 +92,21 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_DEALLOCATE:
 		{
-			const furious::DeallocateRequest& deallocateRequest = request.deallocaterequest();
+			const furious::DeallocateRequest& deallocateRequest = request.deallocate_request();
 			return FJS_Execute_DeAllocate(instance,
 				deallocateRequest.id_a());
 		}
 		case furious::Request_Type_FETCH:
 		{
 			struct FJS_Buffer buffer;
-			const furious::FetchRequest& fetchRequest = request.fetchrequest();
+			const furious::FetchRequest& fetchRequest = request.fetch_request();
 			return FJS_Execute_Fetch(instance,
 				fetchRequest.id_a(),
 				&buffer);
 		}
 		case furious::Request_Type_BINARY_OPERATION:
 		{
-			const furious::BinaryOperationRequest& binaryOperationRequest = request.binaryoperationrequest();
+			const furious::BinaryOperationRequest& binaryOperationRequest = request.binary_operation_request();
 			return FJS_Execute_BinaryOperation(instance,
 				static_cast<enum FJS_BinaryOperationType>(binaryOperationRequest.type()),
 				binaryOperationRequest.id_a(),
@@ -115,7 +115,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_BINARY_CONST_OPERATION:
 		{
-			const furious::BinaryConstOperationRequest& binaryConstOperationRequest = request.binaryconstoperationrequest();
+			const furious::BinaryConstOperationRequest& binaryConstOperationRequest = request.binary_const_operation_request();
 			return FJS_Execute_BinaryConstOperation(instance,
 				static_cast<enum FJS_BinaryConstOperationType>(binaryConstOperationRequest.type()),
 				binaryConstOperationRequest.id_a(),
@@ -124,7 +124,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_UNARY_OPERATION:
 		{
-			const furious::UnaryOperationRequest& unaryOperationRequest = request.unaryoperationrequest();
+			const furious::UnaryOperationRequest& unaryOperationRequest = request.unary_operation_request();
 			return FJS_Execute_UnaryOperation(instance,
 				static_cast<enum FJS_UnaryOperationType>(unaryOperationRequest.type()),
 				unaryOperationRequest.id_a(),
@@ -132,7 +132,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_REDUCTION_OPERATION:
 		{
-			const furious::ReductionRequest& reductionRequest = request.reductionrequest();
+			const furious::ReductionRequest& reductionRequest = request.reduction_request();
 			return FJS_Execute_Reduction(instance,
 				static_cast<enum FJS_ReductionType>(reductionRequest.type()),
 				reductionRequest.id_a(),
@@ -140,7 +140,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_AXIS_REDUCTION_OPERATION:
 		{
-			const furious::AxisReductionRequest& axisReductionRequest = request.axisreductionrequest();
+			const furious::AxisReductionRequest& axisReductionRequest = request.axis_reduction_request();
 			return FJS_Execute_AxisReduction(instance,
 				static_cast<enum FJS_AxisReductionType>(axisReductionRequest.type()),
 				axisReductionRequest.id_a(),
@@ -149,7 +149,7 @@ extern "C" enum FJS_Error FJS_DispatchRequest(PP_Instance instance, const void* 
 		}
 		case furious::Request_Type_DOT_OPERATION:
 		{
-			const furious::DotOperationRequest& dotOperationRequest = request.dotoperationrequest();
+			const furious::DotOperationRequest& dotOperationRequest = request.dot_operation_request();
 			return FJS_Execute_DotOperation(instance,
 				dotOperationRequest.id_a(),
 				dotOperationRequest.id_b(),
