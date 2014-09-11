310e71b106e2c5c2e95e90bf1daae3b9551dfa4b
Marat Dukhan <maratek@gmail.com>
2014-09-11 19:28:48 -0400

    Configuration: move protoc generation to Ninja

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,13 +90,10 @@ module.exports = function(grunt) {
 		},
 
 		shell: {
-			protoc: {
-				command: "protoc --proto_path=protobuf --cpp_out=lib/nacl protobuf/Requests.proto protobuf/Responses.proto"
-			},
 			configure: {
 				command: "python configure.py"
 			},
-			buildPNaCl: {
+			build: {
 				command: "ninja"
 			}
 		}
@@ -109,6 +106,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-shell");
 
-	grunt.registerTask("default", ["mochaTest", "jshint", "browserify", "uglify", "yuidoc", "shell:protoc", "shell:configure", "shell:buildPNaCl"]);
+	grunt.registerTask("default", ["mochaTest", "jshint", "browserify", "uglify", "yuidoc", "shell:configure", "shell:build"]);
 	grunt.registerTask("test", ["mochaTest"]);
 };
--- configure.py
+++ configure.py
@@ -7,8 +7,12 @@ import sys
 import glob
 import ninja_syntax
 
+def replace_ext(filename, ext):
+    return os.path.splitext(filename)[0] + ext
+
 if __name__ == '__main__':
     parser = optparse.OptionParser()
+    parser.add_option("--with-protoc", dest="protoc", default="protoc")
     parser.add_option("--with-nacl-sdk", dest="nacl_sdk", default=os.getenv("NACL_SDK_ROOT"))
     options, _ = parser.parse_args()
 
@@ -18,7 +22,7 @@ if __name__ == '__main__':
         ninja = ninja_syntax.Writer(buildfile)
 
         # Variables
-        ninja.variable('nacl_sdk_dir', os.path.join(options.nacl_sdk))
+        ninja.variable('nacl_sdk_dir', options.nacl_sdk)
         if sys.platform == 'win32':
             ninja.variable('pnacl_toolchain_dir', '$nacl_sdk_dir/toolchain/win_pnacl')
             ninja.variable('pnacl_cc', '$pnacl_toolchain_dir/bin/pnacl-clang.bat')
@@ -37,6 +41,7 @@ if __name__ == '__main__':
         else:
             print("Unsupported platform: " + sys.platform, file=sys.stderr)
             exit(1)
+        ninja.variable('protoc', options.protoc)
 
         # Rules
         ninja.rule('COMPILE_PNACL_C', '$pnacl_cc -o $out -c $in -MMD -MF $out.d $optflags $cflags',
@@ -51,14 +56,27 @@ if __name__ == '__main__':
             description='CXXLD[PNaCl] $out')
         ninja.rule('FINALIZE_PNACL', '$pnacl_finalize -o $out $in',
             description='FINALIZE[PNaCl] $out')
+        ninja.rule('PROTOC_CXX', '$protoc --proto_path=$indir --cpp_out=$outdir $in',
+            description='PROTOC[CXX] $in')
 
         # Build targets
+        proto_dir = os.path.join(root_dir, "protobuf")
+        proto_sources = [os.path.join(proto_dir, path) for path in glob.glob(os.path.join(proto_dir, "*.proto"))]
         c_source_dir = os.path.join(root_dir, "lib", "nacl")
         c_build_dir = os.path.join(root_dir, "build", "nacl")
         c_sources = [os.path.join(root_dir, path) for path in glob.glob(os.path.join(c_source_dir, "*.c"))]
+        c_objects = [os.path.join(c_build_dir, replace_ext(os.path.relpath(path, c_source_dir), ".bc")) for path in c_sources]
         cxx_sources = [os.path.join(root_dir, path) for path in glob.glob(os.path.join(c_source_dir, "*.cc"))]
-        c_objects = [os.path.join(c_build_dir, os.path.splitext(os.path.relpath(path, c_source_dir))[0]) + ".bc" for path in c_sources]
-        cxx_objects = [os.path.join(c_build_dir, os.path.splitext(os.path.relpath(path, c_source_dir))[0]) + ".bc" for path in cxx_sources]
+        cxx_objects = [os.path.join(c_build_dir, replace_ext(os.path.relpath(path, c_source_dir), ".bc")) for path in cxx_sources]
+        cxx_proto_sources = [os.path.join(c_source_dir, replace_ext(os.path.relpath(path, proto_dir), ".pb.cc")) for path in proto_sources]
+        cxx_proto_headers = [os.path.join(c_source_dir, replace_ext(os.path.relpath(path, proto_dir), ".pb.h")) for path in proto_sources]
+        cxx_proto_objects = [os.path.join(c_build_dir, replace_ext(os.path.relpath(path, c_source_dir), ".bc")) for path in cxx_proto_sources]
+        for proto_source, cxx_source, cxx_header, cxx_object in zip(proto_sources, cxx_proto_sources, cxx_proto_headers, cxx_proto_objects):
+            ninja.build([cxx_source, cxx_header], "PROTOC_CXX", proto_source,
+                variables={'indir': proto_dir, 'outdir': c_source_dir})
+            if cxx_source not in cxx_sources:
+                cxx_sources.append(cxx_source)
+                cxx_objects.append(cxx_object)
         for source, object in zip(c_sources, c_objects):
             ninja.build(object, 'COMPILE_PNACL_C', source,
                 variables={'optflags': '-O3',
